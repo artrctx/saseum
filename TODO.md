e4bad567935e35de3aaf6632f1cb666602f9d80a
artrctx <artrctx@gmail.com>
2026-05-03 13:20:39 -0500

    add in base impl

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO
+
+### Goal:
+
+- Build CLI tool to implement vectoring functionlity with pgvector and mysql Vector
+
+### Tools:
+
+- Cobra & Bubbletea
--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"github.com/spf13/cobra"
+)
 
 // https://github.com/charmbracelet/bubbletea/tree/main
 // https://github.com/spf13/cobra
+// https://github.com/pgvector/pgvector/tree/master
+// https://www.youtube.com/watch?v=a4HBKEda_F8&t=15s
+// https://cobra.dev/docs/examples/02-task-manager/
+
+var rootCmd = &cobra.Command{
+	Use:   "saseum",
+	Short: "Implement search vector for using your own thing",
+	Long:  `Implement search vector for using your own thing`,
+}
+
+func init() {
+
+}
+
 func main() {
-	fmt.Println("DEER")
+	if err := rootCmd.Execute(); err != nil {
+		panic(err)
+	}
 }
